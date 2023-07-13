cca21f986e199297bd1cdddbec5a86dea03b1cbd
Shayan Shiari <shayanshiari@gmail.com>
2023-07-13 22:20:17 +0430

    bugs fixed

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/logo.png" />
+        <title>CarDeals</title>
+      </Head>
       <body className="dark:bg-[#23272f] antialiased bg-[#f8f9fa]">
         <Main />
         <NextScript />
--- pages/cars/[carId].js
+++ pages/cars/[carId].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import carsData from "@/data/carsData";
 import CarDetails from "@/components/templates/CarDetails";
 
-const carDetail = () => {
+const CarDetail = () => {
   const router = useRouter();
   const { carId } = router.query;
   const carDetails = carsData[carId - 1];
@@ -10,4 +10,4 @@ const carDetail = () => {
   return <CarDetails {...carDetails} />;
 };
 
-export default carDetail;
+export default CarDetail;
